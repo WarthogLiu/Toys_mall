6edb50cbc221729cbad6f33e325e91bc0c9e2a47
Warthog <liuziyang531579131@outlook.com>
2020-02-17 16:37:18 +0800

    change-password

--- models/db_connect.js
+++ models/db_connect.js
@@ -1,5 +1,5 @@
 // 数据库连接地址
 
-const db_connect = 'mongodb+srv://admin:8638938lzY@mymongodb-rdfvx.azure.mongodb.net/test?retryWrites=true&w=majority'
+const db_connect = 'mongodb+srv://guest:iJewL2J8ACoo3Xe5@mymongodb-rdfvx.azure.mongodb.net/test?retryWrites=true&w=majority'
 
 module.exports = db_connect
